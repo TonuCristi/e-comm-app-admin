fdc75962450f7a89b7727186dc37b83c3256b19a
Tonu Cristian <chrisdev2002@gmail.com>
2024-03-17 17:29:57 +0200

    some changes

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -6,39 +6,67 @@ const userSchema = mongoose.Schema(
   {
     username: {
       type: String,
-      required: [true, "Please enter a name"],
-      minlength: [6, "Minimum name length is 6 characters"],
+      required: true,
     },
     email: {
       type: String,
-      required: [true, "Please enter an email"],
+      required: true,
       unique: true,
-      validate: [validator.isEmail, "Please enter a valid email"],
     },
     password: {
       type: String,
-      required: [true, "Please enter a password"],
-      minlength: [6, "Minimum password length is 6 characters"],
+      required: true,
     },
     role: {
       type: String,
-      required: [true, "Please enter a role"],
+      required: true,
     },
   },
   { timestamps: true }
 );
 
 // fire function after doc saved to db
-userSchema.post("save", function (doc, next) {
-  console.log("New user was created and saved", doc);
-  next();
-});
+// userSchema.post("save", function (doc, next) {
+//   console.log("New user was created and saved", doc);
+//   next();
+// });
 
 // fire function before doc saved to db
-userSchema.pre("save", async function (next) {
-  const salt = await bcrypt.genSalt();
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
-});
+// userSchema.pre("save", async function (next) {
+//   const salt = await bcrypt.genSalt();
+//   this.password = await bcrypt.hash(this.password, salt);
+//   next();
+// });
+
+// static signup method
+userSchema.statics.signup = async function (newUser) {
+  const { username, email, password, role } = newUser;
+
+  // username validation
+  if (!username) throw new Error("Please enter a name");
+  if (username.length < 6)
+    throw new Error("Minimum name length is 6 characters");
+
+  // email validation
+  const exists = await this.findOne({ email });
+  if (!email) throw new Error("Please enter an email");
+  if (!validator.isEmail(email)) throw new Error("Please enter a valid email");
+  if (exists) throw new Error("This email is already taken");
+
+  // password validation
+  if (!password) throw new Error("Please enter a password");
+  if (!validator.isStrongPassword(password))
+    throw new Error("Password not strong enough");
+
+  // role validation
+  if (!role) throw new Error("Please enter a role");
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  const user = await this.create({ username, email, password: hash, role });
+
+  return user;
+};
 
 export const User = mongoose.model("User", userSchema);
--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -3,43 +3,16 @@ import { User } from "../models/userModel.js";
 
 const router = express.Router();
 
-// handle errors
-function handleErrors(err) {
-  const errors = { username: "", email: "", password: "", role: "" };
-
-  // duplicate error code
-  if (err.code === 11000) {
-    errors.email = "This email is already taken";
-    return errors;
-  }
-
-  // validation errors
-  if (err.message.toLowerCase().includes("user validation failed")) {
-    Object.values(err.errors).forEach(({ properties: { path, message } }) => {
-      errors[path] = message;
-    });
-  }
-
-  return errors;
-}
-
-router.get("/signup", (req, res) => {
-  return res.status(200).json("signup");
-});
-
-router.get("/login", (req, res) => {
-  return res.status(200).json("login");
-});
-
 router.post("/signup", async (req, res) => {
-  const { username, email, password, role } = req.body;
-
   try {
-    const user = await User.create({ username, email, password, role });
-    res.status(201).json(user);
+    // const user = await User.create({ username, email, password, role });
+    // res.status(201).json(user);
+
+    const user = await User.signup(req.body);
+    res.status(200).json(user);
   } catch (err) {
-    const errors = handleErrors(err);
-    res.status(400).send(errors);
+    // const errors = handleErrors(err);
+    res.status(400).send(err.message);
   }
 });
 
